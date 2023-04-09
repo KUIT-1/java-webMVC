16cd00629e096a4a5252ea7bbd4e1cd88941d201
kwiyoon <jiyun0245@gmail.com>
2023-04-09 22:25:16 +0900

    feat: Enhance security for access to user(step1-5)
    session을 이용하여 list 접근 및 사용자 정보 수정 제한

--- src/main/java/jwp/controller/ListUserController.java
+++ src/main/java/jwp/controller/ListUserController.java
@@ -9,12 +9,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/userList")
 public class ListUserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String userId = (String) session.getAttribute("userId");
+
+        // 리스트 확인 제한 -> 로그인 요청
+        if(userId == null){
+            resp.sendRedirect("/user/login.jsp");
+            return;
+        }
+
         req.setAttribute("users", MemoryUserRepository.getInstance().findAll());
         RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
         rd.forward(req,resp);
