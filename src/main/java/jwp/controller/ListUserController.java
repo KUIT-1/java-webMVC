918320b0c3691303df4658ef6dccaf295bd772f6
hhj <gudwn357@gmail.com>
2023-04-22 22:54:11 +0900

    feat (ListUserController) : session을 이용한 인가 처리

--- src/main/java/jwp/controller/ListUserController.java
+++ src/main/java/jwp/controller/ListUserController.java
@@ -8,12 +8,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/list")
 public class ListUserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+
+        if (value == null) {
+            resp.sendRedirect("/user/login.jsp");
+            return;
+        }
+
         req.setAttribute("users", MemoryUserRepository.getInstance().findAll());
         RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
         rd.forward(req,resp);
