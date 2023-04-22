7068c00e1d52055911a49a03643b7f7fd15f5c15
hhj <gudwn357@gmail.com>
2023-04-22 22:54:36 +0900

    feat (UpdateUserFormController) : session을 이용한 인가 처리

--- src/main/java/jwp/controller/UpdateUserFormController.java
+++ src/main/java/jwp/controller/UpdateUserFormController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/updateForm")
@@ -16,7 +17,20 @@ public class UpdateUserFormController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 세션에 저장된 정보 가져오기
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+        if (value == null) {
+            resp.sendRedirect("/user/login.jsp");
+            return;
+        }
+        User sessionUser = (User) value;
         User userById = MemoryUserRepository.getInstance().findUserById(req.getParameter("userId"));
+
+        if (!userById.isSameUser(sessionUser.getUserId())) {
+            resp.sendRedirect("/user/list");
+            return;
+        }
 
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("/user/updateForm.jsp");
         req.setAttribute("user", userById);
