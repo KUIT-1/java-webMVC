197767830eafe03b14f7679f4f552be3caa06e58
hhj <gudwn357@gmail.com>
2023-04-26 22:48:32 +0900

    feat (Controller) : create

---
+++ src/main/java/jwp/controller/Controller.java
@@ -0,0 +1,9 @@
+package jwp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Controller {
+
+    String execute(HttpServletRequest req, HttpServletResponse resp);
+}
