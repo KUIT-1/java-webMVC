0f80119912f4e74ce8f219f4eb0f87f8223e475f
hhj <gudwn357@gmail.com>
2023-05-02 21:32:31 +0900

    feat (Question) : create

---
+++ src/main/java/jwp/model/Question.java
@@ -0,0 +1,23 @@
+package jwp.model;
+
+import java.util.Date;
+
+public class Question {
+
+    private int questionId;
+    private String writer;
+    private String title;
+    private String contents;
+    private Date createdDate;
+    private int countOfAnswer;
+
+    public Question(int questionId, String writer, String title, String contents, Date createdDate, int countOfAnswer) {
+        this.questionId = questionId;
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+        this.createdDate = createdDate;
+        this.countOfAnswer = countOfAnswer;
+    }
+
+}
