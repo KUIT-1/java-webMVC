f4d085539a21029977821a5b2c91e0198e5e5a77
hhj <gudwn357@gmail.com>
2023-05-02 21:47:39 +0900

    feat (HomeController) : add list to model

--- src/main/java/jwp/controller/HomeController.java
+++ src/main/java/jwp/controller/HomeController.java
@@ -1,14 +1,22 @@
 package jwp.controller;
 
 import core.mvc.Controller;
+import jwp.dao.QuestionDao;
+import jwp.model.Question;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 
 public class HomeController implements Controller {
+
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+
+        List<Question> list = new QuestionDao().findAll();
+        req.setAttribute("questions", list);
+
         return "/home.jsp";
     }
 }
