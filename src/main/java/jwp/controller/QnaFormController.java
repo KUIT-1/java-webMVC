25bd85a609f8d3426e19badc7d75b595097587f2
hhj <gudwn357@gmail.com>
2023-05-03 22:48:54 +0900

    feat (QnaFormController) : setAttribute Question to request
    questionId 파라미터가 존재할 경우 jdbc에서 조회 한 값을, 없을 경우 빈 객체를 저장 -> form.jsp 에서 질문 생성, 수정 모두 처리하기 위함 (왜 이렇게 했지..)

--- src/main/java/jwp/controller/QnaFormController.java
+++ src/main/java/jwp/controller/QnaFormController.java
@@ -1,6 +1,8 @@
 package jwp.controller;
 
 import core.mvc.Controller;
+import jwp.dao.QuestionDao;
+import jwp.model.Question;
 import jwp.util.UserSessionUtils;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +17,15 @@ public class QnaFormController implements Controller {
         if (!UserSessionUtils.isLogined(session)) {
             return "/user/loginForm";
         }
+
+        String questionId = req.getParameter("questionId");
+        if (questionId != null) {
+            Question question = new QuestionDao().findById(Integer.parseInt(questionId));
+            req.setAttribute("question", question);
+        } else {
+            req.setAttribute("question", new Question("", "", ""));
+        }
+
         return "/qna/form.jsp";
     }
 }
