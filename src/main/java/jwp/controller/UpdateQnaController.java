9d38d637a9e0b73a04966003c6dba965e6921d5d
hhj <gudwn357@gmail.com>
2023-05-03 22:53:00 +0900

    feat (UpdateQnaController) : create
    인가 처리, 본인의 질문만 수정 가능하도록 처리, Question 수정

---
+++ src/main/java/jwp/controller/UpdateQnaController.java
@@ -0,0 +1,40 @@
+package jwp.controller;
+
+import core.mvc.Controller;
+import jwp.dao.QuestionDao;
+import jwp.model.Question;
+import jwp.model.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class UpdateQnaController implements Controller {
+
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        HttpSession session = req.getSession();
+        Object user = session.getAttribute("user");
+        if (user == null) {
+            return "/user/loginForm";
+        }
+
+        User loginUser = (User) user;
+        String questionId = req.getParameter("questionId");
+
+        Question question = new QuestionDao().findById(Integer.parseInt(questionId));
+        if (!loginUser.getName().equals(question.getWriter())) {
+            throw new IllegalArgumentException();
+        }
+
+        question.update(
+                req.getParameter("writer"),
+                req.getParameter("title"),
+                req.getParameter("contents")
+        );
+
+        new QuestionDao().update(question);
+
+        return "redirect:/";
+    }
+}
