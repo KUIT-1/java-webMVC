3309cbb588c9bfbf80339cc3f7f5c5974c2b3ea0
kwiyoon <jiyun0245@gmail.com>
2023-05-05 01:45:24 +0900

    feat: Create Question model&Dao

---
+++ src/main/java/jwp/dao/QuestionDao.java
@@ -0,0 +1,25 @@
+package jwp.dao;
+
+import core.jdbc.JdbcTemplate;
+import jwp.model.Question;
+import jwp.model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class QuestionDao {
+
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+    public List<Question> findAll() throws SQLException {
+        String sql = "SELECT * FROM QUESTIONS";
+        return jdbcTemplate.query(sql,
+                rs-> new Question(
+                        rs.getInt("questionId"),
+                        rs.getString("writer"),
+                        rs.getString("title"),
+                        rs.getString("contents"),
+                        rs.getDate("createdDate"),
+                        rs.getInt("countOfAnswer")));
+    }
+}
---
+++ src/main/java/jwp/model/Question.java
@@ -0,0 +1,87 @@
+package jwp.model;
+
+import java.sql.Date;
+import java.util.Objects;
+
+public class Question {
+    private int questionId;
+    private String writer;
+    private String title;
+    private String contents;
+    private Date createdDate;
+    private int countOfAnswer;
+
+    public Question(int questionId, String writer, String title, String contents, Date createdDate, int countOfAnswer) {
+        this.questionId = questionId;
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+        this.createdDate = createdDate;
+        this.countOfAnswer = countOfAnswer;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public int getCountOfAnswer() {
+        return countOfAnswer;
+    }
+
+    public boolean isSameUser(User user) {
+        return writer.equals(user.getUserId());
+    }
+
+    public void updateTitleAndContents(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+    public void increaseCountOfAnswer() {
+        countOfAnswer++;
+    }
+
+    public void decreaseCountOfAnswer() {
+        countOfAnswer--;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getWriter(), getTitle(), getContents(), getCountOfAnswer());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Question question = (Question) obj;
+        return getCountOfAnswer() == question.getCountOfAnswer()
+                && Objects.equals(getWriter(), question.getWriter())
+                && Objects.equals(getTitle(), question.getTitle())
+                && Objects.equals(getContents(), question.getContents());
+    }
+
+    @Override
+    public String toString() {
+        return "Question [questionId=" + questionId + ", writer=" + writer + ", title=" + title + "]";
+    }
+}
