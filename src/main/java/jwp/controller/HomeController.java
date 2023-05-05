095f834310a9bd0865876abad46dd7974d37aa29
137pjy <pjy137@konkuk.ac.kr>
2023-05-05 15:02:45 +0900

    feat: 1단계 질문게시판 구현 - READ

--- src/main/java/jwp/controller/HomeController.java
+++ src/main/java/jwp/controller/HomeController.java
@@ -1,14 +1,19 @@
 package jwp.controller;
 
 import core.mvc.Controller;
+import jwp.dao.QuestionDao;
+import jwp.util.UserSessionUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
 public class HomeController implements Controller {
+    QuestionDao questionDao=new QuestionDao();
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+
+        req.setAttribute("questions", questionDao.findAll());
         return "/home.jsp";
     }
 }
